b993cbcc094af3d86fa477eb459265dcaab3c6bd
HuangWei <market@zuiyue.com>
2023-09-07 12:03:43 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
+    let instance = single_instance::SingleInstance::new("wei-ui")?;
+    if !instance.is_single() { 
+        std::process::exit(1);
+    };
+
     println!("Hello, world!");
 }
