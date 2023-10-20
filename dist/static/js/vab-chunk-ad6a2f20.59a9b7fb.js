5ae5972b23a4502d796bc658162bb40295b80ced
HuangWei <market@zuiyue.com>
2023-10-20 11:26:32 +0800

    update wei-log

--- dist/static/js/vab-chunk-ad6a2f20.59a9b7fb.js
+++ dist/static/js/vab-chunk-ad6a2f20.59a9b7fb.js
@@ -1,7 +1,7 @@
 /*!
  *  build: Vue  Admin Plus
  *  copyright: vue-admin-beautiful.com
- *  time: 2023-10-16 10:23:30
+ *  time: 2023-10-19 19:31:27
  */
 (self["webpackChunkadmin_plus"]=self["webpackChunkadmin_plus"]||[]).push([[5409],{42152:function(t){
 /*!
